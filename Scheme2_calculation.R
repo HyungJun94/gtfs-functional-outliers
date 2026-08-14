50718db385deccf48cc32d757c58001d323c1704
HyungJun94 <hlim1410@korea.ac.kr>
2026-08-14 21:21:08 +0900

    scheme2 calculation code updated

--- Scheme2_calculation.R
+++ Scheme2_calculation.R
@@ -1,11 +1,10 @@
-#' GTFS variant (Scheme1)
+#' GTFS variant (Scheme2)
 #'
-#' This file contains the implementation of the GTFS variant 'Scheme1' 
+#' This file contains the implementation of the GTFS variant 'Scheme2' 
 #' for functional shape outlier detection.
 #'
 #' @author Hyungjun Lim
 #' @import fda
-#' @import coga 
 #' @name scheme1_core
 NULL
 
@@ -17,14 +16,16 @@ NULL
 #' @param N sample size
 #' @param nbasis number of basis functions for smoothing
 #' @param nharm number of eigenfunctions to be estimated in FPCA
+#' @param adjust eigenvalue adjustment described in the manuscript
+#' @param re.adjust additional adjustment option
 #' @param v.prop proportion of total variation expected to be explained by d eigenfunctions
 #' 
-#' @return A list containing the statistic (delta.H), number of incorporated eigenfunctions (r), FPCA raw result (fpca), and integral-centered function (e.c).
+#' @return A list containing the statistic (delta.H), number of incorporated eigenfunctions (r), FPCA raw result (fpca).
 #' @export
-delta.calculation <- function(xfd, H, N, nbasis, nharm, v.prop){
+D.calculation <- function(xfd, H, N, nbasis, nharm, adjust=T, re.adjust=F , v.prop){
   # Final delta statistic calculation 
   xfd.sub <- xfd$fd[H]                      # final subset fd objects
-  mean.sub <- mean.fd(xfd.sub)              # mean function estimate 
+  mean.sub <- fda::mean.fd(xfd.sub)              # mean function estimate 
   xfd.c <- xfd                              # e_i = X_i - mu0_hat
   xfd.c$fd$coefs <- xfd.c$fd$coefs - matrix(rep(mean.sub$coefs,N), ncol=N)  # update e_i coefficients
   
@@ -35,67 +36,32 @@ delta.calculation <- function(xfd, H, N, nbasis, nharm, v.prop){
   
   # Evaluate eigenvalues 
   e.c.pc <- fda::pca.fd(e.c$fd[H], nharm=nharm)              # conduct FPCA
-  eval <- e.c.pc$values                                 # eigenvalues
+  eval <- e.c.pc$values                                 # eigen values
   r <- min(which(cumsum(eval)/sum(eval) >= v.prop))     # number of eigenvalues explaining pre-specified proportion of total variation
   
   # Find cutoff D-version
   e.c.scores <- fda::inprod(e.c$fd, e.c.pc$harmonics[1:r])
-  delta.H <- apply(e.c.scores,1, function(x){sum(x^2)})  
+  delta.H <- apply(e.c.scores,1, function(x){sum(x^2/eval[1:r])})  
   
-  out <- list(delta.H =delta.H, r=r, fpca=e.c.pc, e.c = e.c)
+  if(adjust == T){
+    # adjusting constant theta 
+    medi = qchisq(0.5, d=r)                           # theoretical median  
+    if(re.adjust == T){
+      theta <- median(delta.H[H])/medi                         # adjusting constant
+    }else{
+      theta <- median(delta.H)/medi                         # adjusting constant
+    }
+    eval <- eval * theta  
+  }
+  
+  # Re-evaluate delta with adjusted eigenvalues
+  delta.H <- apply(e.c.scores,1, function(x){sum(x^2/eval[1:r])}) 
+  
+  out <- list(delta.H =delta.H, r=r, fpca=e.c.pc)
   return(out)
 }
 
 
-#' Finding a threshold incorporating the chi-square convolution 
-#'
-#' @param r number of incorporated eigenfunctions
-#' @param ec.pc FPCA result obtained from centered data
-#' @param alpha expected false positive rate for threshold 
-#' @param delta.H calculated scores 
-#' @param H subset indice
-#' @param adjust eigenvalue adjustment described in the manuscript
-#' @param re.adjust additional adjustment option
-#' 
-#' @return list containing the cutoff value and the density estimate for the scores
-#' @export
-find_cutoff <- function(r, ec.pc , alpha, delta.H, H, adjust=T, re.adjust=F){
-  
-  eval <- ec.pc$values
-  
-  seq0 <- seq(0,1,len=7000)                                      # sequence for a grid search
-  
-  # adjusting constant theta 
-  if(adjust==T){
-    # finding median
-    i <- 1                                                       # sequence index 
-    prob <- 0
-    while(prob <= 0.5){
-      i = i + 1
-      prob = coga::pcoga(seq0[i], rep(1/2, r), 1/2/eval[1:r])  
-    }
-    medi <- seq0[i]                                              # theoretical median  
-    if(re.adjust==T){
-      theta <- median(delta.H[H])/medi                           # adjusting constant
-    }else{
-      theta <- median(delta.H)/medi                              # adjusting constant
-    }
-    eval <- eval * theta                                         # adjusted eigenvalues
-  }
-  
-  # find cutoff   (coga : R package for convolution of gamma(chi-square) distribution)
-  i <- 1 
-  prob <- 0                                                
-  while(prob <= 1-alpha){
-    i = i +1  
-    prob = coga::pcoga(seq0[i], rep(1/2, r), 1/2/eval[1:r])                  # find a cutoff point that suits the significance level 
-  }
-  cutoff <- seq0[i]                                                    # cutoff point
-  density <- coga::dcoga(seq(0,2, len=300), rep(1/2, r), 1/2/eval[1:r])      # density values for visualization 
-  result <- list(cutoff = cutoff, 
-                 density = density)
-  return(result)
-}
 
 
 #' Iteration step function 
@@ -111,8 +77,13 @@ find_cutoff <- function(r, ec.pc , alpha, delta.H, H, adjust=T, re.adjust=F){
 #'  
 #' @return A list containing the clean subset indices (H1), sum of calculated scores (sum.delta.H), calculated scores (delta.H).
 #' @export
-Iter.step.delta <- function(xfd,N,H,h,nbasis,v.prop=0.9, nharm=10, init.pca){
-
+Iter.step.D <- function(xfd,N,H,h,nbasis,v.prop=0.9, nharm=10, init.pca){
+  # xfd : x converted to fd object
+  # N : sample size 
+  # H : subset index
+  # h : subset size 
+  # basis.c : constant basis function
+  
   xfd.sub <- xfd$fd[H]              # subset fd objects
   mean.sub <- fda::mean.fd(xfd.sub)      # mean estimate from the subset
   xfd.c <- xfd                      # centered x subset fd objects   
@@ -122,16 +93,15 @@ Iter.step.delta <- function(xfd,N,H,h,nbasis,v.prop=0.9, nharm=10, init.pca){
   e.c <- xfd.c                      # centered e_i = X_i - mu0_hat 
   e.c$fd$coefs <- xfd.c$fd$coefs - matrix(rep(ei.bar,rep(nbasis,N)),ncol=N)  # update centered e_i coefficients
   
-  
   eval <- init.pca$values
   efunc <- init.pca$harmonics
   
   r <- min(which(cumsum(eval)/sum(eval) >= v.prop))  # number of eigenvalues explaining pre-specified proportion of total variation
-  e.c.pc.scores <- fda::inprod(e.c$fd, efunc[,1:r])
+  e.c.pc.scores <- fda::inprod(e.c$fd, efunc)
   if(r == 1){
-    delta.H <- (e.c.pc.scores[,1])^2
+    delta.H <- (e.c.pc.scores[,1])^2/eval[1]
   }else{
-    delta.H <- apply(e.c.pc.scores[,1:r],1, function(x){sum(x^2)})  
+    delta.H <- apply(e.c.pc.scores[,1:r],1, function(x){sum(x^2/eval[1:r])})  # divided by eigenvalues
   }
   
   H1 <- order(delta.H)[1:h]               # updated subset (with the smallest sum of delta)
@@ -143,7 +113,9 @@ Iter.step.delta <- function(xfd,N,H,h,nbasis,v.prop=0.9, nharm=10, init.pca){
 
 
 
-#' GTFS scheme1 calculation function 
+
+
+#' GTFS scheme2 calculation function 
 #'
 #' @param x N x p functional data
 #' @param t sampling time grid points
@@ -158,12 +130,21 @@ Iter.step.delta <- function(xfd,N,H,h,nbasis,v.prop=0.9, nharm=10, init.pca){
 #'  
 #' @return A list containing the clean subset (H), calculated statistic (delta.H), indices labeled outliers (outlier), cutoff value (cutoff), density estimate for scores (density).
 #' @export
-GTFS_scheme1 <- function(x,t,alpha=0.05,n.init=5,iter.max=10,
+GTFS_scheme2 <- function(x,t,alpha=0.05,n.init=5,iter.max=10,
                          basis='bspline', nbasis=31,
                          v.prop=0.9, nharm=10,
                          refined = T){
+  # x : N x P data
+  # t : sampling grid points
+  # h : subsample size
+  # n.init : number of random initial start
+  # iter.max : maximum number of iteration
+  # basis: orthonormal basis for functional approximation ('bspline'/ 'fourier')
+  # v.prop: proportion of variation for d 
+  # nharm: number of eigenfunctions to be estimated in FPCA
+  # refined: whether to conduct one-step refinement step 
   
-  # scaling (for convenience)
+  # scaling (for convenience) 
   x <- x/sd(x)
   
   # Dataset size & settings
@@ -178,37 +159,42 @@ GTFS_scheme1 <- function(x,t,alpha=0.05,n.init=5,iter.max=10,
   }else if(basis=='fourier'){
     basis0 <- fda::create.fourier.basis(c(0,1), nbasis=nbasis)
   }
-  lambdas <- 10^(-(1:10))  # regularization parameter 
+  lambdas <- 10^(-(1:10))  # regularization parameter
   gcvs <- c()              # Generalized Cross Validation Scores
   for(i in 1:10){
     tmp.fd <- fda::smooth.basis(t,t(x),fda::fdPar(basis0,2, lambdas[i]))
     gcvs <- c(gcvs, mean(tmp.fd$gcv))
   }
-  mypar <- fdPar(basis0,2, lambda=lambdas[which.min(gcvs)]) 
+  mypar <- fda::fdPar(basis0,2, lambda=lambdas[which.min(gcvs)]) 
   xfd <- fda::smooth.basis(t,t(x),mypar)  # smoothed functional objects 
   
   # initial eigenfunction 
-  MDP.result <- MDP(2,h,x,N,p)                                               # MDP result
-  delta.cal <- delta.calculation(xfd, MDP.result$Hmdp, N, nbasis, nharm, 
-                                 v.prop=v.prop)                              # calculate delta statistic with MDP subset
-  init.pca <- delta.cal$fpca                                                 # FPCA result
+  MDP.result <- MDP(2,h,x,N,p)                                           # MDP result
+  init.pca <- D.calculation(xfd, MDP.result$Hmdp, N, nbasis, nharm, F,
+                            v.prop=v.prop)   # FPCA
+  init.pca <- init.pca$fpca                                              # eigenfunction
   
   # clean subset extraction step 
   delta.sum.vec <- rep(0,n.init)           # vector of outlyingness measure
   H.out.mat <- matrix(0,nrow=n.init,h)     # vector of final subset indices 
   for(k in 1:n.init){                      # multiple initial starting (n.init) 
-    rand.init <- sample(1:N, 2, replace = F)                             # random initial subset indices (cardinality=2)
-    H.init <- Iter.step.delta(xfd,N, rand.init,h,
-                              nbasis, v.prop=v.prop, nharm=nharm,
-                              init.pca)                                  # undated subset indices
-    H <- H.init$H1 
-    n.iter <- 1                                                          # update iteration count
-    while(n.iter <= iter.max ){                                          # repeat until the maximum iteration
-      H.updated <- Iter.step.delta(xfd,N,rand.init,h,
-                                   nbasis, v.prop=v.prop, nharm=nharm,
-                                   init.pca)                             # undated subset indices
-      if(sum(H == H.updated$H1) == h){                                   # break when subset members don't change by update
-        H.out <- H.updated  
+    rand.init <- sample(1:N, 2)                                        # random initial subset indices (cardinality=2)
+    #t0 <- system.time(
+    H.init <- Iter.step.D(xfd,N,rand.init,h,
+                          nbasis, v.prop=v.prop, nharm=nharm,
+                          init.pca)                                    # undated subset indices
+    #)
+    #print(t0)
+    H <- H.init$H1  
+    n.iter <- 1                                                        # update iteration count
+    while(n.iter <= iter.max ){                                        # until iteration reaches pre-specified maximum
+      #print(n.iter)
+      H.updated <- Iter.step.D(xfd,N,H,h,
+                               nbasis, v.prop=v.prop, nharm=nharm,
+                               init.pca)                               # update subset indices
+      if(sum(H == H.updated$H1) == h){                                 # break when subset members don't change by update
+        H.out <- H.updated                                   
+        n.iter <- n.iter + 1
         break
       }else{
         H = H.updated$H1                                  
@@ -226,48 +212,38 @@ GTFS_scheme1 <- function(x,t,alpha=0.05,n.init=5,iter.max=10,
   delta.sum <- delta.sum.vec[ind.H2]        # final sum of deltas 
   
   # statistic calculation 
-  delta.result <- delta.calculation(xfd, H2, N, nbasis, nharm, v.prop=v.prop)
-  delta.H <- delta.result$delta.H
-  r <- delta.result$r
-  e.c <- delta.result$e.c
-  ec.pc <- delta.result$fpca
-  
-  # Find cutoff according to pre-specified significance level alpha
-  cutoff.result <- find_cutoff(r, ec.pc, alpha, delta.H, H2,
-                               adjust=T)
-  cutoff <- cutoff.result$cutoff
-  outlier <- which(delta.H > cutoff) 
-  
-  result <- list(H=H2,delta.H=delta.H, delta.sum=delta.sum,
-                 outlier=outlier, cutoff=cutoff,
-                 density = cutoff.result$density)
-  
+  D.result <- D.calculation(xfd, H2, N, nbasis, nharm, T,v.prop=v.prop)
+  delta.H <- D.result$delta.H
+  r <- D.result$r
   
   # Refined procedure
   if(refined==T){
-    cutoff.result <- find_cutoff(r, ec.pc, alpha/2, delta.H, H2,
-                                 adjust=T)
-    cutoff <- cutoff.result$cutoff
+    cutoff <- qchisq(1-alpha/2, df=r) 
     HR <- which(delta.H < cutoff)           # indices that are classified as non-outlying observations
     
-    # calculate statistic
-    delta.result.HR <- delta.calculation(xfd, HR, N, nbasis, nharm, v.prop=v.prop)
-    delta.HR <- delta.result.HR$delta.H
-    r.HR <- delta.result.HR$r
-    e.c.HR <- delta.result.HR$e.c
-    ec.pc.HR <- delta.result.HR$fpca
+    D.result <- D.calculation(xfd, HR, N, nbasis, nharm, adjust=T, re.adjust =F,v.prop=v.prop)
+    delta.HR <- D.result$delta.H
+    r.HR <- D.result$r
     
     # Find cutoff according to pre-specified significance level alpha
-    cutoff.result.HR <- find_cutoff(r.HR, ec.pc.HR, alpha, delta.HR, HR,
-                                    adjust=T)
-    cutoff.HR <- cutoff.result.HR$cutoff
-    density.HR <- cutoff.result.HR$density
+    cutoff.HR <- qchisq(1-alpha, df=r.HR)  
+    density.HR <- dchisq(seq(0,50,len=300), df= r.HR)
     outlier.HR <- which(delta.HR > cutoff.HR) 
     
     result <- list(H=HR,delta.H=delta.HR,
                    outlier=outlier.HR, cutoff=cutoff.HR,
                    density = density.HR)
+  }else{
+    # Find cutoff according to pre-specified significance level alpha
+    cutoff <- qchisq(1-alpha, df=r)  
+    density <- dchisq(seq(0,50,len=300), df= r)
+    outlier <- which(delta.H > cutoff) 
+    
+    result <- list(H=H2,delta.H=delta.H, delta.sum=delta.sum,
+                   outlier=outlier, cutoff=cutoff,
+                   density = density)
   }
   
   return(result)
 }
+
