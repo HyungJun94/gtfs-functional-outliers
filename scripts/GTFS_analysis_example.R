644dfa91e6201509f7268bc82aede1da91d3009e
HyungJun94 <hlim1410@korea.ac.kr>
2026-08-23 17:31:48 +0900

    GTFS analysis example code revised

--- scripts/GTFS_analysis_example.R
+++ scripts/GTFS_analysis_example.R
@@ -20,11 +20,12 @@ x <- add_intercept(x, method='normal_adapt')  # add intercept
 x <- x$raw
 
 system.time(
-  trial.C <- GTFS(x,t,alpha=0.05, alpha.p= 0.2,n.init=10,iter.max=10,
-                  basis='bspline', nbasis=31,
-                  v.prop=0.9, nharm=15,
-                  refined=T, gumbel.cutoff=0.01,
-                  coeffs.type = 'algorithm')   # algorithm -> Algorithm 1 & practical -> practical ver.
+  trial.C <- GTFS_outlier(scheme='scheme1',   # 'GTFS', 'GTFS(P)', 'scheme1', 'scheme2' 
+                          x,t, 
+                          basis='bspline', nbasis=31, nharm=20, 
+                          v.prop=0.9,alpha=0.05, q.star='default',
+                          n.init=5,iter.max=10,
+                          refined = T)   
 )
 
 trial.C$outlier
@@ -45,73 +46,6 @@ hist(trial.C$delta.H,probability = T,nclass=n/5, ylim=c(0, quantile(trial.C$dens
      main='Hist. of delta & actual dist')
 lines(seq(0,50, len=300), trial.C$density, lwd=2, col='orange')
 abline(v = trial.C$cutoff, lwd=3, col='blue')
-
-
-
-## Scheme1 test run -------------------------------------------------------------
-x <- simul_1(n,p,t,c)
-x <- add_intercept(x, method='normal_adapt')  # add intercept
-x <- x$raw
-
-system.time(
-  trial <- GTFS_scheme1(x,t,alpha=0.05,n.init=10,iter.max=10,
-                        basis='bspline', nbasis=31,
-                        v.prop=0.9, nharm=15,
-                        refined=T)
-)
-
-
-trial$outlier
-length(trial$outlier)
-
-
-## plot
-par(mfrow=c(1,2))
-plot(trial$delta.H, ylab='delta_i(H)', main='delta_i(H) plot')
-points(oind, trial$delta.H[oind], col='red')
-abline(h = trial$cutoff, col='blue', lwd=3)
-
-
-# Check by histogram of delta
-hist(trial$delta.H,probability = T,nclass=n/5, ylim=c(0,max(trial$density)+2),
-     main='Hist. of delta & actual dist')
-lines(seq(0,2, len=300), trial$density, lwd=3, col='orange')
-abline(v = trial$cutoff, lwd=3, col='blue')
-
-
-
-
-## Scheme2 test run -------------------------------------------------------------
-x <- simul_31(n,p,t,c)
-x <- add_intercept(x, method='normal_adapt')  # add intercept
-x <- x$raw
-
-system.time(
-  trial.D <- GTFS_scheme2(x,t,alpha=0.05,n.init=10,iter.max=10,
-                          basis='bspline', nbasis=31,
-                          v.prop=0.9, nharm=15,
-                          refined=T)
-)
-
-trial.D$outlier
-length(trial.D$outlier)
-
-## plot
-par(mfrow=c(1,2))
-plot(trial.D$delta.H, ylab='delta_i(H)', main='delta_i(H) plot')
-points(oind, trial.D$delta.H[oind], col='red')
-abline(h = trial.D$cutoff, col='blue', lwd=3)
-
-
-# Check by histogram of delta
-hist(trial.D$delta.H,probability = T,nclass=n/5, ylim=c(0, max(trial.D$density)+0.1),
-     main='Hist. of delta & actual dist')
-lines(seq(0,50, len=300), trial.D$density, lwd=2, col='orange')
-abline(v = trial.D$cutoff, lwd=3, col='blue')
-
-
-
-
 
 
 
